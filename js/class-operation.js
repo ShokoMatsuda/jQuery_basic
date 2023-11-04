7b664c46b7586c3c38c78d01500787637ad63951
ShokoMatsuda <syokom.0128@gmail.com>
2023-11-04 23:04:53 +0900

    toggleClass()メソッドを実装

--- js/class-operation.js
+++ js/class-operation.js
@@ -4,4 +4,14 @@ $(function() {
         // id属性がtargetの要素にclass属性'sample'を追加
         $('#target').addClass('sample');
     });
+
+    $('#remove').on('click', function(){
+        // id属性がtargetの要素からclass属性'sample'を削除
+        $('#target').removeClass('sample');
+    });
+
+    $('#toggle').on('click', function(){
+        // id属性がtargetの要素にclass属性'sample'を追加／削除する
+        $('#target').toggleClass('sample');
+    });
 });

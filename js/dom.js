265fd4d767732b58b86c58a97708ddab9047ae3f
ShokoMatsuda <syokom.0128@gmail.com>
2023-11-05 18:39:40 +0900

    7.4 HTML要素を追加しよう

--- js/dom.js
+++ js/dom.js
@@ -1,3 +1,11 @@
 $(function () {
-    $('p').text('jQueryを勉強しよう');
+    // 変数の初期化
+    let num = 0;
+
+    // id属性がappendの要素がクリックされたら要素を追加
+    $('#append').on('click', function(){
+        // numを1ずつ増やす
+        num++;
+        $('ul').append('<li>appendで追加' + num + '</li>');
+    });
 });

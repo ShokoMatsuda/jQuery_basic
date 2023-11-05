78f9529d468475891ef9e79e95ffb29c66775f00
ShokoMatsuda <syokom.0128@gmail.com>
2023-11-05 19:48:49 +0900

    8.5 チェックボックスのチェック数に上限を設けよう

--- js/form.js
+++ js/form.js
@@ -27,4 +27,16 @@ $(function () {
                 $("#check").prop('disabled', ture);
             }
         });
-});
+
+        // 1. 趣味のチェックボックスが変更されたら
+        $('[name="hobby"]').on('change', function () {
+            // 2.、3. チェックの数が3より大きかったら
+            if ($('[name="hobby"]:checked').length > 3) {
+                // 4. チェックを付けようとしたものだけチェックを外す
+                $(this).prop('checked', false);
+
+                // 5. 注意を促すメッセージを表示する
+                alert('3つまでしか選択できません');
+            }
+        });
+    });
